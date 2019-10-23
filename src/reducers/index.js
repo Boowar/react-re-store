89cb5794cabef3adf22513e4b78bec67a06c27e0
boowar <as.yarchuk@gmail.com>
2019-10-23 19:21:56 +0200

    Update array

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,30 @@ const initialState = {
   orderTotal: 0,
 }
 
+const updateCartItem = (book, item = {}) => {
+  const {
+    id = book.id,
+    count = 0,
+    title = book.title,
+    total = book.price,
+  } = item
+
+  return {
+    id,
+    title,
+    count: count + 1,
+    total: total + book.price,
+  }
+}
+
+const updateCartItems = (cartItems, item, idx) => {
+  if (idx === -1) {
+    return [...cartItems, item]
+  }
+
+  return [...cartItems.slice(0, idx), item, ...cartItems.slice(idx + 1)]
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_REQUESTED":
@@ -34,15 +58,13 @@ const reducer = (state = initialState, action) => {
     case "BOOK_ADDED_TO_CART":
       const bookId = action.payload
       const book = state.books.find(book => book.id === bookId)
-      const newItem = {
-        id: book.id,
-        name: book.title,
-        count: 1,
-        total: book.price,
-      }
+      const itemIndex = state.cartItems.findIndex(({ id }) => id === bookId)
+      const item = state.cartItems[itemIndex]
+
+      const newItem = updateCartItem(book, item)
       return {
         ...state,
-        cartItems: [...state.cartItems, newItem],
+        cartItems: updateCartItems(state.cartItems, newItem, itemIndex),
       }
 
     default:
