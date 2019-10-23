9d900b86f5048d99d0f3cb27c9caf68f7a12763a
boowar <as.yarchuk@gmail.com>
2019-10-23 11:03:37 +0200

    Routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,16 @@
 import React from "react"
+import { Route, Switch } from "react-router-dom"
 
 import "./app.css"
+import { HomePage, CardPage } from "../pages"
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <Switch>
+      <Route path='/' component={HomePage} exact />
+      <Route path='/card/' component={CardPage} />
+    </Switch>
+  )
 }
 
 export default App
--- src/components/hoc/with-bookstore-service.js
+++ src/components/hoc/with-bookstore-service.js
@@ -1,19 +1,16 @@
 import React from "react"
-import { BookstoreServiceConsumer } from '../bookstore-service-context'
+import { BookstoreServiceConsumer } from "../bookstore-service-context"
 
-
-const withBookstoreService = () = (Wrapped) => {
-  return (props) => {
+const withBookstoreService = () => Wrapped => {
+  return props => {
     return (
-<BookstoreServiceConsumer>
-  {
-    (bookstoreService) => {
-      return <Wrapped {...props} bookstoreService={bookstoreService}/>
-    }
-  }
-</BookstoreServiceConsumer>
+      <BookstoreServiceConsumer>
+        {bookstoreService => {
+          return <Wrapped {...props} bookstoreService={bookstoreService} />
+        }}
+      </BookstoreServiceConsumer>
     )
-  } 
+  }
 }
 
 export default withBookstoreService
