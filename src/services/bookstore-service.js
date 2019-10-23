fc56d2315c5fa152e2aa183a9466893e69272119
boowar <as.yarchuk@gmail.com>
2019-10-23 15:21:47 +0200

    Add styles

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -5,8 +5,18 @@ export default class BookstoreService {
         id: 1,
         title: "Production-Ready Microservices",
         author: "Susan J. Fowler",
+        price: 32,
+        coverImage:
+          "https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg",
       },
-      { id: 2, title: "Realise It!", author: "Michael T. Nygard" },
+      {
+        id: 2,
+        title: "Realise It!",
+        author: "Michael T. Nygard",
+        price: 35,
+        coverImage:
+          "https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg",
+      },
     ]
   }
 }
