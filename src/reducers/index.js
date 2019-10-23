f686b365274aa363188554ef32833bafdadc6871
boowar <as.yarchuk@gmail.com>
2019-10-23 18:25:09 +0200

    Add in array

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,21 +2,8 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
-  cartItems: [
-    {
-      id: 1,
-      name: "Book 1",
-      count: 3,
-      total: 100,
-    },
-    {
-      id: 2,
-      name: "Book 2",
-      count: 2,
-      total: 200,
-    },
-  ],
-  orderTotal: 300,
+  cartItems: [],
+  orderTotal: 0,
 }
 
 const reducer = (state = initialState, action) => {
@@ -27,6 +14,7 @@ const reducer = (state = initialState, action) => {
         loading: true,
         error: null,
       }
+
     case "FETCH_BOOKS_SUCCESS":
       return {
         ...state,
@@ -34,6 +22,7 @@ const reducer = (state = initialState, action) => {
         loading: false,
         error: null,
       }
+
     case "FETCH_BOOKS_FAILURE":
       return {
         ...state,
@@ -41,6 +30,21 @@ const reducer = (state = initialState, action) => {
         loading: false,
         error: action.payload,
       }
+
+    case "BOOK_ADDED_TO_CART":
+      const bookId = action.payload
+      const book = state.books.find(book => book.id === bookId)
+      const newItem = {
+        id: book.id,
+        name: book.title,
+        count: 1,
+        total: book.price,
+      }
+      return {
+        ...state,
+        cartItems: [...state.cartItems, newItem],
+      }
+
     default:
       return state
   }
